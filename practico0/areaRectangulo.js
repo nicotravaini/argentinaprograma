5f7257a687d9581fb754d202ebd1be317ca4a1ff
Nico Travaini <nicotravaini@gmail.com>
2023-05-23 18:02:09 -0300

    Practico N0 finalizado

---
+++ practico0/areaRectangulo.js
@@ -0,0 +1,4 @@
+var base = 12.5;
+var altura = 5.3;
+var area = base * altura;
+console.log(area);
---
+++ practico0/promedio3.js
@@ -0,0 +1,5 @@
+var n1 = -1.2;
+var n2 = 5.3;
+var n3 = -3.3;
+var promedio = (n1 + n2 + n3) / 3;
+console.log(promedio);

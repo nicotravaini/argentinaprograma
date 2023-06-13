4f71b7fb1cfe2b96023611354669a166145983b3
Nico Travaini <nicotravaini@gmail.com>
2023-06-13 20:50:12 -0300

    Subí el resto del practico1

---
+++ practico1/miNombreObjeto.js
@@ -0,0 +1,2 @@
+let persona = {nombre: "Nicolás", apellido: "Travaini Páez", edad: 31};
+console.log("Mi objeto persona es el siguiente: " + JSON.stringify(persona));
---
+++ practico1/operacionesAritmeticas.js
@@ -0,0 +1,9 @@
+let var1 = 1;
+let var2 = 1;
+let var3 = 1;
+let var4 = 23;
+let suma = var1 + var2;
+let resta = var4 - var3;
+let resultadoFinal = suma * resta;
+let esPar = (resultadoFinal % 2) == 0;
+console.log("Mis variables iniciales fueron: %i, %i, %i y %i. La respuesta a verificar si el resultado final es par es: %s", var1, var2, var3, var4, esPar);

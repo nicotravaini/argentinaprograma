b3bd6f522b6832ec8c84c1bcf9904658460c2ce1
Nico Travaini <nicotravaini@gmail.com>
2023-06-13 20:44:07 -0300

    subo archivo frutas

---
+++ practico1/frutas.js
@@ -0,0 +1,6 @@
+let frutas = [];
+frutas.push("bananas", "manzanas", "peras", "mandarinas", "naranjas");
+let ultima = frutas.pop();
+console.log("La última fruta agregada fue:", ultima);
+let primera = frutas.shift();
+console.log("La primera fruta agregada fue:", primera);

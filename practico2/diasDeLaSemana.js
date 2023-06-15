a6799abf444c97af7f66f8cdff514cb93742ab06
Nico Travaini <nicotravaini@gmail.com>
2023-06-15 12:41:37 -0300

    Subí Ejercicio 4 P2

---
+++ practico2/diasDeLaSemana.js
@@ -0,0 +1,27 @@
+const readlineSync = require('readline-sync');
+const diasSemana = readlineSync.question("Ingrese un numero del 1 al 7: ");
+switch(diasSemana){
+    case "1":
+        console.log("Hoy es lunes");
+        break
+    case "2":
+        console.log("Hoy es martes");
+        break
+    case "3":
+        console.log("Hoy es miércoles");
+        break
+    case "4":
+        console.log("Hoy es jueves");
+        break
+    case "5":
+        console.log("Hoy es viernes");
+        break
+    case "6":
+        console.log("Hoy es sábado");
+        break
+    case "7":
+        console.log("Hoy es domingo");
+        break
+    default:
+        console.log("Número inválido");
+}
